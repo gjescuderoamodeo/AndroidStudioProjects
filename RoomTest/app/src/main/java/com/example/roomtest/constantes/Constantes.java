0585ea7325bdd275814542aa1f30db434afabbb3
gjescudero <gjescuderoamodeo@gmail.com>
2022-12-09 13:45:46 +0100

    RoomTest

---
+++ RoomTest/app/src/main/java/com/example/roomtest/constantes/Constantes.java
@@ -0,0 +1,6 @@
+package com.example.roomtest.constantes;
+
+public class Constantes {
+    public static final String NOMBRE_DB="CoordenadaDB";
+    public static final String NOMBRE_TABLA_POSICION="posicion";
+}
