df30c7ae8dac6332437a31ff884bc26248857938
gjescudero <gjescuderoamodeo@gmail.com>
2023-02-10 12:35:50 +0100

    probarRutas

--- probarGestionRutasFirebase/app/src/main/java/com/example/probargestionrutasfirebase/MainActivity.java
+++ probarGestionRutasFirebase/app/src/main/java/com/example/probargestionrutasfirebase/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.probargestionrutasfirebase;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +23,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> lugar = new HashMap<>();
+        lugar.put("test", "Los Angeles");
+        lugar.put("2", "CA");
+        lugar.put("3", "USA");
+
+        db.collection("Lugar").document("LU")
+                .set(lugar)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
+        //leer
+        db.collection("users")
+                .add(lugar)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
 
     }
 }
